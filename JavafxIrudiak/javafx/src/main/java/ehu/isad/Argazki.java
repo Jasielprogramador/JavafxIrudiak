422775713f59a11155db8c02ae8bdaec4618fb0c
Asi <asierrosa@gmail.com>
2020-09-30 13:07:48 +0200

    first commit

---
+++ JavafxIrudiak/javafx/src/main/java/ehu/isad/Argazki.java
@@ -0,0 +1,30 @@
+package ehu.isad;
+
+import javafx.scene.image.Image;
+
+public class Argazki {
+
+    private String izena;
+    private String irudia;
+
+    public Argazki(String pIzena,String pImage){
+        this.izena=pIzena;
+        this.irudia=pImage;
+    }
+
+    public String getIzena() {
+        return izena;
+    }
+
+    public void setIzena(String izena) {
+        this.izena = izena;
+    }
+
+    public String getIrudia() {
+        return irudia;
+    }
+
+    public void setIrudia(String irudia) {
+        this.irudia = irudia;
+    }
+}
---
+++ JavafxIrudiak/javafx/src/main/java/ehu/isad/ComboBoxExperiments.java
@@ -0,0 +1,120 @@
+
+package ehu.isad;
+
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ComboBoxExperiments extends Application {
+
+    private ImageView imageView = new ImageView();
+    private ComboBox comboBilduma = new ComboBox();
+    private ListView listViewOfArgazki;
+
+    /*    VBox vbox =new VBox(imageView);
+                vbox.setAlignment(Pos.BASELINE_CENTER);
+    //Insets para calibrarlo
+                vbox.setPadding(new Insets(30,0,0,0));*/
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Animaliak");
+
+
+        List<String> bildumak = List.of("abereak", "landareak", "frutak");
+        ObservableList<String> bildumaList = FXCollections.observableArrayList(bildumak);
+        comboBilduma.setItems(bildumaList);
+
+
+        /*Relacionas las imagenes con los nombres y todo esto con tu tipo*/
+
+        Map<String, List<Argazki>> bildumaMap = new HashMap<>();
+
+        bildumaMap.put("abereak", List.of(
+                new Argazki("Elefantea", "elefantea.jpeg"),
+                new Argazki("Txakurra", "txakurra.jpeg"),
+                new Argazki("Untxia", "untxia.png")
+        ));
+
+        Map<String, List<Argazki>> bildumaMap1 = new HashMap<>();
+
+        bildumaMap.put("landareak", List.of(
+                new Argazki("Cactus", "cactus.png"),
+                new Argazki("Landareberdea", "landareberdea.jpeg"),
+                new Argazki("Landarehoria", "landarehoria.jpeg")
+        ));
+
+        Map<String, List<Argazki>> bildumaMap2 = new HashMap<>();
+
+        bildumaMap.put("fruta", List.of(
+                new Argazki("Sandia", "sandia.png"),
+                new Argazki("Sagarra", "sagarra.jpeg"),
+                new Argazki("Fresa", "fresa.jpeg")
+        ));
+
+
+        ObservableList<Argazki> argazkiList = FXCollections.observableArrayList();
+        argazkiList.addAll(bildumaMap.get("abereak"));
+        argazkiList.addAll(bildumaMap1.get("landareak"));
+        argazkiList.addAll(bildumaMap2.get("fruta"));
+
+        listViewOfArgazki = new ListView<>(argazkiList);
+
+        listViewOfArgazki.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (observable.getValue() == null) return;
+
+            String fitx = ((Argazki) observable.getValue()).getIrudia();
+
+            try {
+
+                imageView.setImage(lortuIrudia(fitx /* 48x48 */));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        comboBilduma.getSelectionModel().selectFirst();
+
+        //Te permite escribir
+        comboBilduma.setEditable(true);
+
+        Scene scene = new Scene(comboBilduma, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+    private Image lortuIrudia(String location) throws IOException {
+
+        InputStream is = getClass().getResourceAsStream("/" + location);
+        BufferedImage reader = ImageIO.read(is);
+        return SwingFXUtils.toFXImage(reader, null);
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
